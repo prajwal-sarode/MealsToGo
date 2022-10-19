71aab262729b6a8bd34bf5e167999634048bb6a9
prajwal-sarode <prajwalsarode@outlook.in>
2022-10-19 18:57:45 +0530

    comment

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import {
-  StatusBar,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     // this is the react Fragment Element container
+    // added the android status bar line 10
+
     <>
-      <SafeAreaView style={{ flex: 1 , marginTop: StatusBar.currentHeight}}>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
         <View style={{ padding: 16, backgroundColor: "green" }}>
           <Text> Search </Text>
         </View>
